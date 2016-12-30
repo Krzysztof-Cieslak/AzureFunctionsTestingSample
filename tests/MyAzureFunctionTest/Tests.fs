8aa715c51ca2f6a3f9fe8ba76c00f5eae7c11727
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-12-30 12:41:09 +0100

    Add tests

--- tests/MyAzureFunctionTest/Tests.fs
+++ tests/MyAzureFunctionTest/Tests.fs
@@ -1,15 +1,47 @@
 module Tests
 
 open Expecto
+open MyAzureFunction
+
+open System.Net
+open System.Net.Http
+open System.Web.Http
+open System.Web.Http.Hosting
+open Newtonsoft.Json
+
+
 
 [<Tests>]
 let tests =
-  testList "samples" [
-    testCase "universe exists" <| fun _ ->
-      let subject = true
-      Expect.isTrue subject "I compute, therefore I am."
+  testList "MyAzureFunction" [
+    testCase "Run - good message" <| fun _ ->
+      let body = Newtonsoft.Json.JsonConvert.SerializeObject( {name = "Chris"})
 
-    testCase "should fail" <| fun _ ->
-      let subject = false
-      Expect.isTrue subject "I should fail because the subject is false."
+      use ctn = new StringContent(body)
+      use req = new HttpRequestMessage()
+      req.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+      req.Content <- ctn
+
+      let res = Run(req)
+      let resBody = res.Content.ReadAsStringAsync().Result
+
+      Expect.equal resBody "\"Hello Chris\"" ""
+
+
+
+    testCase "Run - bad message" <| fun _ ->
+      let result =
+        try
+          use ctn = new StringContent("")
+          use req = new HttpRequestMessage()
+          req.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+          req.Content <- ctn
+
+          let res = Run(req)
+          let resBody = res.Content.ReadAsStringAsync().Result
+          true
+        with
+        | _ -> false
+
+      Expect.equal result false ""
   ]
